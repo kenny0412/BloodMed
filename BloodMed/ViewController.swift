b8cd54a240278b71da6724e9f4275d9eb201448f
kenny <kennyc680@gmail.com>
2020-09-14 21:14:23 -0600

    Initial Commit

---
+++ BloodMed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BloodMed
+//
+//  Created by Kenny Cardenas Reyes on 9/14/20.
+//  Copyright © 2020 Kenny Cardenas Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
